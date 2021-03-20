25e10eba071d8ed4c9188bcca10449e4ba279969
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-03-21 00:09:25 +0300

    build: added eclipse project files

--- midterm-project-1/src/tr/edu/iztech/teamstech/Main.java
+++ midterm-project-1/src/tr/edu/iztech/teamstech/Main.java
@@ -9,12 +9,12 @@ import tr.edu.iztech.teamstech.view.MainView;
 public class Main {
     public static void main(String[] args) {
         try {
-        EntityDirector td = new TeamDirector(new CsvDataInitializer(), new CsvDataSaver());
-        td.save();
-        MainView k = new MainView(td);
-        k.show();
-        k.kill();
-        td.save();
+            EntityDirector td = new TeamDirector(new CsvDataInitializer(), new CsvDataSaver());
+            td.save();
+            MainView k = new MainView(td);
+            k.show();
+            k.kill();
+            td.save();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
