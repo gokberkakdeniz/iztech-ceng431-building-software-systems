d79429ad2916c068d8b9b64cfe4f8b56ea02f4de
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-04-10 23:53:28 +0300

    feat: exit/logout mechanism partially implemented

--- midterm-project-2/src/tr/edu/iztech/pma/view/MainView.java
+++ midterm-project-2/src/tr/edu/iztech/pma/view/MainView.java
@@ -2,6 +2,7 @@ package tr.edu.iztech.pma.view;
 
 import tr.edu.iztech.pma.io.KeyboardReader;
 import tr.edu.iztech.pma.view.admin.AdminView;
+import tr.edu.iztech.pma.people.*;
 
 public class MainView extends View{
     public MainView() {
@@ -9,27 +10,50 @@ public class MainView extends View{
     }
 
     public boolean show() {
-        AuthorizationView auth = new AuthorizationView();
-        auth.bindKeyboardReader(keyboardReader);
-        auth.show();
-        String userType = Session.getUser().getClass().getSimpleName();
-        switch (userType) {
-            case "Admin":
-                AdminView view = new AdminView();
-                view.bindKeyboardReader(keyboardReader);
-                view.show();
-                break;
-            case "Employee":
-                System.out.println("You are logged in as Employee!");
-                break;
-            case "Manager":
-                System.out.println("You are logged in as Manager!");
-                break;
+        while (true) {
+            KeyboardReader.Options options = new KeyboardReader.Options(
+                    "Available options",
+                    new String[] {
+                        "Login",
+                        "Exit"
+                    },
+                    true);
+            options.printOptions();
+            int option = keyboardReader.promptInteger("Please select operation", options.getPredicate());
+            switch (option) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    System.out.println("görüşmek üzere");
+                    return true;
+                default:
+                    break;
+            }
         }
-        return true;
     }
 
     public void kill() {
         keyboardReader.close();
+    }
+
+    private void login() {
+        AuthorizationView auth = new AuthorizationView();
+        auth.bindKeyboardReader(keyboardReader);
+        auth.show();
+
+        IPerson user = Session.getUser();
+
+        if (user instanceof Admin) {
+            AdminView view = new AdminView();
+            view.bindKeyboardReader(keyboardReader);
+            view.show();
+        } else if (user instanceof Employee) {
+            System.out.println("You are logged in as Employee!");
+        } else if (user instanceof Manager) {
+            System.out.println("You are logged in as Manager!");
+        }
+
+        Session.setUser(null);
     }
 }
