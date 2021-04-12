0dfa9acf81699cf2db5a0970ccfea9f3e7eb4ade
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-04-12 11:00:42 +0300

    refactor: comments for people package is done

--- midterm-project-2/src/tr/edu/iztech/pma/people/IPerson.java
+++ midterm-project-2/src/tr/edu/iztech/pma/people/IPerson.java
@@ -1,6 +1,18 @@
 package tr.edu.iztech.pma.people;
 
 public interface IPerson {
+    /**
+     * Gets username of person
+     *
+     * @return username
+     */
     String getUsername();
+
+    /**
+     * Checks if authorized
+     * @param password password to be compared
+     *
+     * @return if passwords are equal returns true, otherwise false
+     */
     boolean login(String password);
 }
--- midterm-project-2/src/tr/edu/iztech/pma/people/IPersonnel.java
+++ midterm-project-2/src/tr/edu/iztech/pma/people/IPersonnel.java
@@ -2,6 +2,14 @@ package tr.edu.iztech.pma.people;
 
 import tr.edu.iztech.pma.product.IProduct;
 
+/**
+ * Manager and Employee are Personnel since they have Product/Assembly/Part.
+ */
 public interface IPersonnel {
+    /**
+     * Gets product of person
+     *
+     * @return product
+     */
     IProduct getProduct();
 }
