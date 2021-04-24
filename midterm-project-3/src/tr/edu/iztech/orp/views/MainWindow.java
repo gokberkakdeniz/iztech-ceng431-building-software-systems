a4e0c22925e282785255cb7f12467961ae58eca0
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-04-24 19:11:25 +0300

    mainwindows some impl

--- midterm-project-3/src/tr/edu/iztech/orp/views/MainWindow.java
+++ midterm-project-3/src/tr/edu/iztech/orp/views/MainWindow.java
@@ -6,13 +6,16 @@ import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class MainWindow {
+	private static final String title = "IZTECH Outfit Rating Platform";
 
 	private JFrame frame;
-	private JButton btnNewButton;
 	private JPanel panel;
 	
 	/**
@@ -44,30 +47,46 @@ public class MainWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 960, 720);
-		frame.setTitle("IZTECH Outfit Rating Platform");
+		frame.setTitle(title);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		btnNewButton = new JButton("New button");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					frame.getContentPane().remove(1);
-				} catch (Exception ignored) {
+		JPanel headerPanel = new JPanel();
+		frame.getContentPane().add(headerPanel, BorderLayout.NORTH);
+		headerPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
+		
+		JComboBox<MenuModel> viewCombo = new JComboBox<MenuModel>();
+		viewCombo.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent event) {
+				if (event.getStateChange() == ItemEvent.SELECTED) {
+					System.out.println(event);
 				}
-
-				if (panel instanceof LoginPanel) {
-					panel = new HomePanel();
-				} else {
-					panel = new LoginPanel();
-				}
-				
-				frame.getContentPane().add(panel, BorderLayout.SOUTH);
-				frame.getContentPane().repaint();
-				frame.getContentPane().revalidate();
 			}
 		});
-		frame.getContentPane().add(btnNewButton, BorderLayout.NORTH);
+		viewCombo.setModel(new DefaultComboBoxModel<>(MenuModel.values()));
+		headerPanel.add(viewCombo);
 		
+		JButton logoutButton = new JButton("Logout");
+		headerPanel.add(logoutButton);
+		
+		JPanel contentPanel = new JPanel();
+		frame.getContentPane().add(contentPanel, BorderLayout.CENTER);
+	}
+	
+	private enum MenuModel {
+		HOME("Home"),
+		COLLECTIONS("Collections"),
+		SUBSCRIBED_USERS("Subscribed Users");
+		
+	    private final String text;
+
+	    MenuModel(String text) {
+	        this.text = text;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return text;
+		}
 	}
 }
