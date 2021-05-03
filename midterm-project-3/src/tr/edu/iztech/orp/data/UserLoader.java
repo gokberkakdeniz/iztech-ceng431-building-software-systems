6e1094d3315fab2c3e6e5788908e466faf41ee74
Hakan Alp <hakana926@gmail.com>
2021-05-03 18:49:54 +0300

    UserLoader

--- midterm-project-3/src/tr/edu/iztech/orp/data/UserLoader.java
+++ midterm-project-3/src/tr/edu/iztech/orp/data/UserLoader.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import tr.edu.iztech.orp.enums.OutfitColor;
-import tr.edu.iztech.orp.enums.OutfitGender;
-import tr.edu.iztech.orp.enums.OutfitOccasion;
-import tr.edu.iztech.orp.enums.OutfitSize;
-import tr.edu.iztech.orp.enums.OutfitType;
-import tr.edu.iztech.orp.models.Outfit;
-import tr.edu.iztech.orp.models.OutfitCollection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import tr.edu.iztech.orp.models.User;
 
 public class UserLoader implements IDataLoader<User> {
@@ -27,26 +29,50 @@ public class UserLoader implements IDataLoader<User> {
 	@Override
 	public List<User> load() {
 		List<User> users = new ArrayList<User>();
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			Document document = builder.parse(file);
+			
+			document.getDocumentElement().normalize();
+			NodeList userList = (NodeList) document.getDocumentElement();
+//			NodeList userList = document.getElementsByTagName("users");
+			
+			for(int i = 0; i < userList.getLength(); ++i) {
+				Node node = userList.item(i);
+				System.out.println(node.getTextContent());
+			}
+			
+						 
+//			System.out.println(root.getElementsByTagName("users"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		users.add(new User("",""));
-		users.add(new User("a","a"));
-		users.add(new User("b","b"));
-		OutfitCollection col1 = new OutfitCollection("col1");
-		OutfitCollection col2 = new OutfitCollection("col2");
-		OutfitCollection col3 = new OutfitCollection("col3");
-
-		col1.add(new Outfit(1, "Tshirt", "DeFacto", OutfitGender.MAN, OutfitType.TSHIRT, OutfitOccasion.SPORTY, OutfitColor.BLACK, new OutfitSize[] { OutfitSize.XXXXXL, OutfitSize.M }));
-		col1.add(new Outfit(2, "Tshirt", "Koton", OutfitGender.UNISEX, OutfitType.TSHIRT, OutfitOccasion.CASUAL, OutfitColor.YELLOW, new OutfitSize[] { OutfitSize.S, OutfitSize.M }));
 		
-		col3.add(new Outfit(1, "Tshirt", "DeFacto", OutfitGender.MAN, OutfitType.TSHIRT, OutfitOccasion.SPORTY, OutfitColor.BLACK, new OutfitSize[] { OutfitSize.XXXXXL, OutfitSize.M }));
-		col3.add(new Outfit(2, "Tshirt", "Koton", OutfitGender.UNISEX, OutfitType.TSHIRT, OutfitOccasion.CASUAL, OutfitColor.YELLOW, new OutfitSize[] { OutfitSize.S, OutfitSize.M }));
 		
-		users.get(0).addCollection(col1);
-		users.get(0).addCollection(col2);
-		users.get(1).addCollection(col3);
 		
-		users.get(0).follow(users.get(1));
-		users.get(1).followedBy(users.get(0));
+//		users.add(new User("",""));
+//		users.add(new User("a","a"));
+//		users.add(new User("b","b"));
+//		OutfitCollection col1 = new OutfitCollection("col1");
+//		OutfitCollection col2 = new OutfitCollection("col2");
+//		OutfitCollection col3 = new OutfitCollection("col3");
+//
+//		col1.add(new Outfit(1, "Tshirt", "DeFacto", OutfitGender.MAN, OutfitType.TSHIRT, OutfitOccasion.SPORTY, OutfitColor.BLACK, new OutfitSize[] { OutfitSize.XXXXXL, OutfitSize.M }));
+//		col1.add(new Outfit(2, "Tshirt", "Koton", OutfitGender.UNISEX, OutfitType.TSHIRT, OutfitOccasion.CASUAL, OutfitColor.YELLOW, new OutfitSize[] { OutfitSize.S, OutfitSize.M }));
+//		
+//		col3.add(new Outfit(1, "Tshirt", "DeFacto", OutfitGender.MAN, OutfitType.TSHIRT, OutfitOccasion.SPORTY, OutfitColor.BLACK, new OutfitSize[] { OutfitSize.XXXXXL, OutfitSize.M }));
+//		col3.add(new Outfit(2, "Tshirt", "Koton", OutfitGender.UNISEX, OutfitType.TSHIRT, OutfitOccasion.CASUAL, OutfitColor.YELLOW, new OutfitSize[] { OutfitSize.S, OutfitSize.M }));
+//		
+//		users.get(0).addCollection(col1);
+//		users.get(0).addCollection(col2);
+//		users.get(1).addCollection(col3);
+//		
+//		users.get(0).follow(users.get(1));
+//		users.get(1).followedBy(users.get(0));
 
 		return users;
 	}
