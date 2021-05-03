6c2e6f921295874c72c649117b3d4804b57a2448
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-05-03 22:22:16 +0300

    feat: easy login added

--- midterm-project-3/src/tr/edu/iztech/orp/views/screens/LoginPanel.java
+++ midterm-project-3/src/tr/edu/iztech/orp/views/screens/LoginPanel.java
@@ -12,6 +12,9 @@ import javax.swing.SwingConstants;
 
 import tr.edu.iztech.orp.controllers.LoginController;
 import tr.edu.iztech.orp.exceptions.LoginFailedException;
+import javax.swing.JTextPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Description:
@@ -47,7 +50,7 @@ public class LoginPanel extends JPanel {
 		passwordLabel.setBounds(334, 204, 77, 15);
 		add(passwordLabel);
 		
-		passwordInput = new JPasswordField();
+		passwordInput = new JPasswordField("123456");
 		passwordInput.setBounds(439, 202, 212, 19);
 		add(passwordInput);
 		passwordInput.setColumns(10);
@@ -62,6 +65,99 @@ public class LoginPanel extends JPanel {
 		messageLabel.setForeground(Color.RED);
 		messageLabel.setBounds(334, 268, 317, 15);
 		add(messageLabel);
+		
+		ezUsernames1 = new JTextField();
+		ezUsernames1.setToolTipText("Click for set username input");
+		ezUsernames1.setText("alp");
+		ezUsernames1.setEditable(false);
+		ezUsernames1.setBounds(158, 348, 114, 19);
+		add(ezUsernames1);
+		ezUsernames1.setColumns(10);
+		
+		ezUsernames2 = new JTextField();
+		ezUsernames2.setToolTipText("Click for set username input");
+		ezUsernames2.setText("akdeniz");
+		ezUsernames2.setEditable(false);
+		ezUsernames2.setColumns(10);
+		ezUsernames2.setBounds(284, 348, 114, 19);
+		add(ezUsernames2);
+		
+		ezUsernames3 = new JTextField();
+		ezUsernames3.setToolTipText("Click for set username input");
+		ezUsernames3.setText("johannes");
+		ezUsernames3.setEditable(false);
+		ezUsernames3.setColumns(10);
+		ezUsernames3.setBounds(410, 348, 114, 19);
+		add(ezUsernames3);
+		
+		ezUsernames4 = new JTextField();
+		ezUsernames4.setToolTipText("Click for set username input");
+		ezUsernames4.setText("huseyin");
+		ezUsernames4.setEditable(false);
+		ezUsernames4.setColumns(10);
+		ezUsernames4.setBounds(537, 348, 114, 19);
+		add(ezUsernames4);
+		
+		ezUsernames5 = new JTextField();
+		ezUsernames5.setToolTipText("Click for set username input");
+		ezUsernames5.setText("tennant");
+		ezUsernames5.setEditable(false);
+		ezUsernames5.setColumns(10);
+		ezUsernames5.setBounds(663, 348, 114, 19);
+		add(ezUsernames5);
+		
+		JLabel ezUsernamesLabel = new JLabel("Valid Usernames:");
+		ezUsernamesLabel.setBounds(158, 321, 398, 15);
+		add(ezUsernamesLabel);
+		
+		JLabel ezPasswordLabel = new JLabel("Password:");
+		ezPasswordLabel.setBounds(158, 379, 145, 15);
+		add(ezPasswordLabel);
+		
+		ezPassword = new JTextField();
+		ezPassword.setToolTipText("Click for set username password");
+		ezPassword.setText("123456");
+		ezPassword.setEditable(false);
+		ezPassword.setColumns(10);
+		ezPassword.setBounds(158, 406, 114, 19);
+		add(ezPassword);
+		
+		lblYouCanSelect = new JLabel("Note: You can select inputs or click them to set set username/password input.");
+		lblYouCanSelect.setForeground(Color.BLUE);
+		lblYouCanSelect.setBounds(158, 446, 619, 15);
+		add(lblYouCanSelect);
+		
+		// NOTE: THOSE ARE FOR EASY TESTING!
+		ezUsernames1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				usernameInput.setText(ezUsernames1.getText());
+			}
+		});
+		ezUsernames2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				usernameInput.setText(ezUsernames2.getText());
+			}
+		});
+		ezUsernames3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				usernameInput.setText(ezUsernames3.getText());
+			}
+		});
+		ezUsernames4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				usernameInput.setText(ezUsernames4.getText());
+			}
+		});
+		ezUsernames5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				usernameInput.setText(ezUsernames5.getText());
+			}
+		});
 	}
 	
 	public void showError() {
@@ -86,4 +182,11 @@ public class LoginPanel extends JPanel {
             }
         }
     };
+	private JTextField ezUsernames1;
+	private JTextField ezUsernames2;
+	private JTextField ezUsernames3;
+	private JTextField ezUsernames4;
+	private JTextField ezUsernames5;
+	private JTextField ezPassword;
+	private JLabel lblYouCanSelect;
 }
