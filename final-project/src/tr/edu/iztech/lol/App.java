4fdcab88678aab911563b4ef1f583f5519015675
Hakan Alp <hakana926@gmail.com>
2021-05-18 23:00:08 +0300

    fix: resolve conflict

--- final-project/src/tr/edu/iztech/lol/App.java
+++ final-project/src/tr/edu/iztech/lol/App.java
@@ -1,11 +1,27 @@
 package tr.edu.iztech.lol;
 
+import tr.edu.iztech.lol.factory.*;
 import tr.edu.iztech.lol.hero.*;
 import tr.edu.iztech.lol.view.MainWindow;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("sfsd");
-		new MainWindow();
+		// new MainWindow();
+		
+		IHeroFactory ds = new DragonSlayerFactory();
+		IHero dsas = ds.createAssasin();
+		
+		IHeroFactory lb = new LightbringerFactory();
+		IHero lbca = lb.createCavalier();
+		
+		System.out.println(lbca);
+
+		IState m = dsas.attack(lbca.getState());
+		IState g = lbca.defend(m);
+		g.setHealthPoint(g.getHealthPoint() - g.getDamageDealt());
+		g.setDamageDealt(0);
+		lbca.setState(g);
+				
+		System.out.println(lbca);
 	}
 }
