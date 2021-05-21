b057c3706c34a7b2259d4cba8a20d3e1bf1776b6
Hakan Alp <hakana926@gmail.com>
2021-05-21 22:41:44 +0300

    refactor: move serialize methods into save class

--- final-project/src/tr/edu/iztech/lol/data/IDeserializer.java
+++
@@ -1,7 +0,0 @@
-package tr.edu.iztech.lol.data;
-
-import tr.edu.iztech.lol.model.IModel;
-
-public interface IDeserializer<T extends IModel> {
-	T deserizalize(String s);
-}
--- final-project/src/tr/edu/iztech/lol/data/ISerizalizer.java
+++
@@ -1,7 +0,0 @@
-package tr.edu.iztech.lol.data;
-
-import tr.edu.iztech.lol.model.IModel;
-
-public interface ISerizalizer<T extends IModel> {
-	String serizalize(T model);
-}
